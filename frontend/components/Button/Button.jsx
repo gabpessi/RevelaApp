f4d083a58ac867de25eea83469ad708a61c9ff4a
Gabriel Pessi <gabpessi@hotmail.com>
2025-06-30 20:22:21 -0300

    botao

--- frontend/components/Button/Button.jsx
+++ frontend/components/Button/Button.jsx
@@ -1,11 +1,13 @@
 import styles from './Button.module.css';
 
-function Button({ children, onClick, type = 'button', className = '' }) {
+function Button({ children, onClick, type = 'button', className = '', disabled, style }) {
   return (
     <button
       type={type}
       onClick={onClick}
       className={`${styles.button} ${className}`}
+      disabled={disabled}
+      style={style}
     >
       {children}
     </button>
